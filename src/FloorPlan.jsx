27e6e71b1029f7a946518e7cc28315c09876046e
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-10-10 20:07:02 -0400

    set up FloorPlan component and export

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,12 @@
+const FloorPlan = (props) => {
+  return (
+    <>
+      <div>Kitchen</div>
+      <div>LivingRoom</div>
+      <div>Bedroom</div>
+      <div>Bath</div>
+    </>
+  )
+}
+
+export default FloorPlan;
